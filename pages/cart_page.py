480ee145fda37ab38936b8d07039f2fb3354d712
HarmanSmith <leonel.diaz.16842@gmail.com>
2021-12-13 11:56:26 -0300

    testing multiple items

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -4,20 +4,46 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class CartPage:
 
-    price_locator = (By.XPATH, "//div[contains(@class, 'inventory_item_price')]")
+    prices_locator = (By.XPATH, "//div[contains(@class, 'inventory_item_price')]")
     checkout_locator = (By.ID, "checkout")
+    remove_buttons_locator = (By.XPATH, "//button[contains(@class, 'btn btn_secondary btn_small cart_button')]")
 
     def __init__(self, browser):
         self.browser = browser
 
-    def check_price(self, price):
+    def check_single_price(self, price):
         same_price = False
-        element = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located(self.price_locator))
-        if price == float((element.text).lstrip('$')):
+        element = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located(self.prices_locator))
+        if price == float(element.text.lstrip('$')):
             same_price = True
         else:
             same_price = False
         return same_price
 
+    def check_price_in_list(self, price, index):
+        same_price = False
+        #element = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located(self.prices_locator))
+        elements = WebDriverWait(self.browser, 10).until(EC.presence_of_all_elements_located(self.prices_locator))
+
+        if price == float(elements[index].text.lstrip('$')):
+            same_price = True
+        else:
+            same_price = False
+        return same_price
+
+    # todo check if ok
+    def check_two_prices(self, price1, price2):
+        test_ok = False
+        price1_ok = self.check_price_in_list(price1, 0)
+        price2_ok = self.check_price_in_list(price2, 1)
+        if price1_ok == True & price2_ok == True:
+            test_ok = True
+        else:
+            test_ok = False
+        return test_ok
+
+    def click_remove_item(self, index):
+        self.browser.find_elements_by_id(self.remove_buttons_locator)[index].click()
+
     def click_checkout(self):
-        pass
+        WebDriverWait(self.browser, 10).until(EC.presence_of_element_located(self.checkout_locator)).click()
--- tests/test_remove_cart.py
+++ tests/test_remove_cart.py
@@ -1,11 +1,10 @@
-# from behave import *
 from user import StandardUser
 from pages.login_page import LoginPage
 from pages.items_page import ItemsPage
 from pages.cart_page import CartPage
 
 
-def test_shopping_cart(browser):
+def test_cart_remove(browser):
     # Background: username and password required
     # Given username and password
     user = StandardUser()
@@ -25,12 +24,16 @@ def test_shopping_cart(browser):
     items_page.sort_items("Price (low to high)")
     # Then the items are sorted by price
     assert items_page.check_items_sorted_lowest()
-    # When the user adds the cheapest item to the cart
+    # When the user adds the cheapest two items to the cart
     cheapest_price = items_page.get_item_price(0)
+    second_cheapest_price = items_page.get_item_price(1)
     items_page.add_to_cart(0)
+    items_page.add_to_cart(1)
     # And clicks in the cart icon
     items_page.click_cart()
     # Then the cart page loads with the selected item
     cart_page = CartPage(browser)
-    assert cart_page.check_single_price(cheapest_price)
-    cart_page.click_checkout()
+    assert cart_page.check_two_prices(cheapest_price, second_cheapest_price)
+    # When the user removes an item from the cart
+
+    # Then the item is effectively removed
